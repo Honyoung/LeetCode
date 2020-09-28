b336e8ab963e07b778529377fb6bc3abced4f831
jensen.liu <jensen.liu@huolala.cn>
2020-09-28 18:57:01 +0800

    Initial commit

---
+++ com/jensen/leetcode/CombineInteger.py
@@ -0,0 +1,35 @@
+#!/bin/usr/python3
+
+from itertools import combinations
+import numpy as np
+
+
+def combineInteger():
+    """
+
+组合数
+
+给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+​
+示例:
+输入: n = 4, k = 2
+输出:
+[
+[2,4],
+[3,4],
+[2,3],
+[1,2],
+[1,3],
+[1,4],
+]
+
+    """
+    i = input("Input two integers n and k:")
+    print(i)
+    s = np.arange(1,int(i[0])+1)
+    print(list(combinations(s,int(i[2]))))
+    print('test')
+
+
+def run():
+    combineInteger()
---
+++ com/jensen/main.py
@@ -0,0 +1,28 @@
+#!/bin/usr/python3
+from com.jensen.leetcode import CombineInteger as ci
+
+
+class Fab(object):
+
+    def __init__(self, max):
+        self.max = max
+        self.n, self.a, self.b = 0, 0, 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.n < self.max:
+            r = self.b
+            self.b, self.b = self.b, self.a + self.b
+            self.n = self.n + 1
+            return r
+        raise StopIteration()
+
+
+if __name__ == '__main__':
+    print('开始运行程序')
+
+    # ci.run()
+    for n in Fab(5):
+        print(n)
